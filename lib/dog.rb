0e9f732c9049680e4ebbd9473740c5423c00265c
Reinald Reynoso <reireynoso@gmail.com>
2019-04-03 02:23:12 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog 
+  
+  def initialize(name)
+    @name = name
+  end
+  def name(name)
+    @name
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+  
+end
